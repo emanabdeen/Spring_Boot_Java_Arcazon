451d1dcaa49bba3a29ecc106d818641ed8b90fff
Eman Abdeen <eabdeen3804@conestogac.on.ca>
2025-04-10 14:59:22 -0400

    first commit

---
+++ src/main/java/com/conestoga/arcazon/ArcazonApplication.java
@@ -0,0 +1,13 @@
+package com.conestoga.arcazon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcazonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArcazonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/conestoga/arcazon/ArcazonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conestoga.arcazon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcazonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
